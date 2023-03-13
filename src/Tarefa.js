304b15dc285af45db5d3b7d0630f63a571e776b6
LeandroGazoli <leandro.gazoli@outlook.com>
2023-03-13 17:54:17 -0300

    finalizando projeto

---
+++ src/Tarefa.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function Tarefa({ data, deLete }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <FontAwesome
+          name="trash"
+          size={20}
+          color="#22272e"
+          onPress={deLete}
+        />
+      </TouchableOpacity>
+      <Text>{data.item}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(169,169,169, 0.20)',
+    marginTop: 12,
+    padding: 12,
+    borderRadius: 4,
+    flexDirection: 'row',
+  },
+  button: {
+    marginRight: 8,
+  },
+});
